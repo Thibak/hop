73ba9857fdd19c349e7507876460c898ca714927
Thibak <russinow.m@gmail.com>
2014-06-02 17:33:24 +0400

    Запустил

--- cell.py
+++ cell.py
@@ -3,7 +3,10 @@
 import numpy
 import copy
 import matplotlib as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+from numpy import arange
+from matplotlib.mlab import griddata
 
 def move(a,b,i): #перемещение из одного списка в другой
     a.append(i)
@@ -84,8 +87,8 @@ class core:
     def __init__(self):
         self.CurClone = clone()
         self.track = []
-   # def newClone(self,s)
-#        self.CurClone = clone(s)"""
+    def newClone(self,s):
+        self.CurClone = clone(s)
     def iterate(self,n,s=None):
         self.product = []       
         self.result = []
@@ -130,12 +133,17 @@ class programm:
          [[[a(i),k] for i in range(4)] for k in range(4)]   
     """
     
-    def __init__(self,f="default"):
+    def __init__(self,itr = 100):
         self.element = []
         self.block = False
-        if f == "default":
-            self.itr = 1000
-            self.dict = {'a1':1,'a2':1,'l1':1,'l2':1}
+        self.itr = itr
+        self.dict = {'a1':1,'a2':1,'l1':1,'l2':1}
+        self.isCalc = False
+        
+    def chPrec(self, n):
+        self.isCalc = False
+        self.itr = n
+        
         
     def push(self):
         self.block = True
@@ -152,57 +160,109 @@ class programm:
             self.dict[name]=value
         else:
             print ('No such name')
-    def keys(self): self.dict.keys()
+    def keys(self): return self.dict.keys()
     def makeDict(self, a1=1,a2=1,l1=1,l2=1):
         return self.dict
         
     # высокоуровневые операции по формированию 
     def makeGrid(self, name1, start1, stop1, step1, name2, start2, stop2, step2):
-        a = frange(start1, stop1, step1)
-        b = frange(start2, stop2, step2)
-        for j in a:
-            for i in b:
-                self.chAttr(name1,j)
-                self.chAttr(name2,i)
-                self.push()
+        self.isCalc = False
+        if name1 in self.dict.keys() and name2 in self.dict.keys():
+            a = arange(start1, stop1, step1)
+            b = arange(start2, stop2, step2)
+            for j in a:
+                for i in b:
+                    self.chAttr(name1,j)
+                    self.chAttr(name2,i)
+                    self.push()
+        else:
+            print ('No such name. Grid creation aborted')
   
 #        [[[self.addDict(i),k] for i in range(4)] for k in range(4)]  
     def makeVec(self, name, start, stop, step):
-        a = frange(start, stop, step)
-        for i in a:
-            self.chAttr(name,i)
-            self.push()
+        self.isCalc = False
+        if name in self.dict.keys():
+            a = arange(start, stop, step)
+            for i in a:
+                self.chAttr(name,i)
+                self.push()
+        else:
+            print ('No such name')
   
         
 
 class experiment:
     """ 
     Объект эксперимент
+        задачи: написать инициализацию по словарю
+        написать вытакивалку имен параметров и результатов
     """
     def __init__(self, p = None):
         self.c = core()
+        
         if p == None:
-            p = programm('default')
-            p.addDict(p.makeDict())
-    def do(self,p):
-        for e in p:
-            self.c.iterate(e)
+            self.p = programm()
+            
+    def setDict(self,p):
+        self.p = p
+    def do(self):
+        self.p.isCalk=True
+        i = 0
+        for s in self.p.element:
+            i=i+1
+            print("calculating " + str(i) + ' from ' + str(len(self.p.element)))
+            print s
+            self.c.iterate(self.p.itr,s)
+            s['MeanRes'] = self.c.GetMeanRes()
+            s['MedRes'] = self.c.GetMedRes()
+            s['MeanProd'] = self.c.GetMeanProd()
+            s['MedProd'] = self.c.GetMedProd()
+        
             
             
-    def plot(self, param = 'result'):
+    def plot(self, resName, param1, param2 = None, t = 'p'):
+        '''
+        Если 1 параметр передается, то рисуется 2D график, если два, то 3D       
+        
+        Для интерполяции неструктурированых данных (кои дефакто мы и имеем, хотя это не совсем так) 
+         надо использовать функцию from matplotlib.mlab import griddata. 
+         Например как тут: http://matplotlib.org/examples/pylab_examples/griddata_demo.html
+         Размерность сетки интерполяции мб такой же как и у данных
+        '''
         # сначала надо вытащить из словаря итерируемые значения
-        x = [self.p.element[i][param] for i in range(len(self.p.element))] 
-        # а -- идентификатор итерируемого значения
+        #if resName = 'all'        
         
-        # надо все результаты тоже загонять в словарь!!! И вытаскивать по той же конструкции
-        
-        # затем 
-        fig = plt.figure()
-        axes = Axes3D(fig)
-        #axes.scatter3D(a,a,a)
-        axes.plot(x,y,z)
-        #axes.plot_wireframe(x, y, z)
-        fig.show()
+        if param2 == None:
+            y = [self.p.element[i][resName] for i in range(len(self.p.element))]
+            x = [self.p.element[i][param1] for i in range(len(self.p.element))] 
+            plt.pyplot.plot(x, y, 'yo-')
+            plt.pyplot.show()
+        else:
+            z = [self.p.element[i][resName] for i in range(len(self.p.element))]
+            x = [self.p.element[i][param1] for i in range(len(self.p.element))] 
+            y = [self.p.element[i][param2] for i in range(len(self.p.element))] 
+            fig = plt.pyplot.figure()
+            axes = Axes3D(fig)
+            
+            axes.set_xlabel(param1)
+            axes.set_ylabel(param2)
+            axes.set_zlabel(resName)            
+            
+            axes.scatter3D(x,y,z, c=z)
+            
+            xi = numpy.linspace(0,1,100)
+            yi = numpy.linspace(0,1,100)
+            # grid the data.
+            zi = griddata(x,y,z,xi,yi,interp='linear')
+
+           # axes.plot_wireframe(X, Y, Z)
+            axes.plot_surface(xi,yi,zi)
+            
+            
+        #    plt.pyplot.colorbar()
+            #axes.plot(x,y,z)
+            #axes.plot_wireframe(x, y, z)
+            fig.show()
 
 
 
