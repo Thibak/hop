3531323aebe597486f21cbad5fe67a76e75da742
Thibak <russinow.m@gmail.com>
2014-08-14 18:26:23 +0400

    проработка XML

--- hop/xmli.py
+++ hop/xmli.py
@@ -5,16 +5,17 @@ Created on Tue Jul 29 15:32:42 2014
 @author: russinow
 
 Синтаксис фабрики:
->>> from lxml.builder import ElementMaker
->>> E = ElementMaker()
->>> d = E.e
->>> ff = E('ddd')
+from lxml.builder import ElementMaker
+E = ElementMaker()
+d = E.e
+ff = E('ddd')
 
 * Для XML-фабрики есть волшебная функция, выполняющая роль сериализатора списка. Т.к. фабрика понимает на входе как строку так и эелемент типа _element, то функция может спокойно использовать в построении фабрики.
->>> def a(l, f):
-...  for i in range(len(l)):
-...   f.append(l[i])
-...  return f
+Фишка в том, что тут передается элементы XMLя, у которого есть атрибут .append
+def a(l, f):
+  for i in range(len(l)):
+   f.append(l[i])
+  return f
 ggg = d(a([d('1'),d('2')], d()))
 
 Для пуша объектов в память используем str(), для извлечения используем eval()
@@ -22,20 +23,36 @@ ggg = d(a([d('1'),d('2')], d()))
 
 Извлечение элементов
 
->>> from lxml import etree
->>> etree.tostring(ff)
-
+from lxml import etree
+etree.tostring(ff)
 
 """
 
-
-
 import xml.etree.ElementTree as ET
+from lxml.builder import ElementMaker
+from lxml import etree #<-- вспомогательная функция для сериализации
 
+E = ElementMaker()
+
+
+def glue(l, f):
+    """
+    Склеить элементы из l функцией f
+    Причем, вектор элементов может содержать любые элементы
+    """
+    for i in range(len(l)):
+        f.append(l[i])
+    return f
 
 def load(name):
     tree = ET.parse(load) #(file_in)
     root = tree.getroot()
     return root
-    
+
+def save():
+    pass
+def 
+
+
+  
     
